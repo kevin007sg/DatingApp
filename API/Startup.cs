f9d616c2ee1973d8c629ea4a19a0f473b9b31617
kevin007sg <44340691+kevin007sg@users.noreply.github.com>
2021-05-25 23:24:05 +0800

    Updated connection

--- API/Startup.cs
+++ API/Startup.cs
@@ -58,8 +58,8 @@ namespace API
 
             app.UseCors(x => x.AllowAnyHeader()
                 .AllowAnyMethod()
-                .AllowCredentials()
-                .WithOrigins("https://localhost:4200"));
+                .AllowCredentials());
+                //.WithOrigins("https://localhost:4200"));
 
             app.UseAuthentication();
             app.UseAuthorization();
